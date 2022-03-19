ffe0e7b098e158a5056d7470bf85890d49c3aa52
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2022-03-19 01:52:30 -0500

    Se agrega el componente de proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     AboutComponent,
-    PresentationComponent
+    PresentationComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
